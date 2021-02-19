f2e5d7c89d799b8e00aa811f32345849b59e929e
Glider2355 <62125008+Glider2355@users.noreply.github.com>
2021-02-19 16:53:15 +0900

    Update settings.py

--- KUglider_platform/settings.py
+++ KUglider_platform/settings.py
@@ -135,11 +135,10 @@ LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 
-"""
 # 暗号化されたhttpsを使うようにする
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
-"""
+
 AUTH_USER_MODEL = 'website.User'
 
 # ローカル時
