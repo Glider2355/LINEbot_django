2d001a23f658d9a50947ca97c39621de8c8e734c
Glider2355 <62125008+Glider2355@users.noreply.github.com>
2021-03-15 10:22:46 +0900

    Update urls.py

--- KUglider_platform/urls.py
+++ KUglider_platform/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_title = '匿名ブログ 内部管理サイト'
-admin.site.site_header = '匿名ブログ 内部管理サイト'
+admin.site.site_title = '内部管理サイト'
+admin.site.site_header = '内部管理サイト'
 admin.site.index_title = 'メニュー'
 # admin.site.unregister(Group)
 
